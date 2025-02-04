b5d4aa007f1fe2fb9a614c57c2ceb5488168b30c
BradyNolin <nolin01@allegheny.edu>
2025-02-04 15:58:02 -0500

    Changing Indent to Tab

--- public/src/client/topic/delete-posts.js
+++ public/src/client/topic/delete-posts.js
@@ -1,104 +1,104 @@
 'use strict';
 
 define('forum/topic/delete-posts', [
-    'postSelect', 'alerts', 'api',
+	'postSelect', 'alerts', 'api',
 ], function (postSelect, alerts, api) {
-    const DeletePosts = {};
-    let modal;
-    let deleteBtn;
-    let purgeBtn;
-    let tid;
+	const DeletePosts = {};
+	let modal;
+	let deleteBtn;
+	let purgeBtn;
+	let tid;
 
-    DeletePosts.init = function () {
-        tid = ajaxify.data.tid;
+	DeletePosts.init = function () {
+		tid = ajaxify.data.tid;
 
-        $(window).off('action:ajaxify.end', onAjaxifyEnd).on('action:ajaxify.end', onAjaxifyEnd);
+		$(window).off('action:ajaxify.end', onAjaxifyEnd).on('action:ajaxify.end', onAjaxifyEnd);
 
-        if (modal) {
-            return;
-        }
+		if (modal) {
+			return;
+		}
 
-        loadModal();
-    };
+		loadModal();
+	};
 
-    function loadModal() {
-        app.parseAndTranslate('modals/delete-posts', {}, function (html) {
-            modal = html;
+	function loadModal() {
+		app.parseAndTranslate('modals/delete-posts', {}, function (html) {
+			modal = html;
 
-            $('body').append(modal);
+			$('body').append(modal);
 
-            initializeModalElements();
-            initializePostSelect();
-            showPostsSelected();
-        });
-    }
+			initializeModalElements();
+			initializePostSelect();
+			showPostsSelected();
+		});
+	}
 
-    function initializeModalElements() {
-        deleteBtn = modal.find('#delete_posts_confirm');
-        purgeBtn = modal.find('#purge_posts_confirm');
+	function initializeModalElements() {
+		deleteBtn = modal.find('#delete_posts_confirm');
+		purgeBtn = modal.find('#purge_posts_confirm');
 
-        modal.find('#delete_posts_cancel').on('click', closeModal);
+		modal.find('#delete_posts_cancel').on('click', closeModal);
 
-        deleteBtn.on('click', function () {
-            deletePosts(deleteBtn, pid => `/posts/${pid}/state`);
-        });
-        purgeBtn.on('click', function () {
-            deletePosts(purgeBtn, pid => `/posts/${pid}`);
-        });
-    }
+		deleteBtn.on('click', function () {
+			deletePosts(deleteBtn, pid => `/posts/${pid}/state`);
+		});
+		purgeBtn.on('click', function () {
+			deletePosts(purgeBtn, pid => `/posts/${pid}`);
+		});
+	}
 
-    function initializePostSelect() {
-        postSelect.init(function () {
-            checkButtonEnable();
-            showPostsSelected();
-        });
-    }
+	function initializePostSelect() {
+		postSelect.init(function () {
+			checkButtonEnable();
+			showPostsSelected();
+		});
+	}
 
-    console.log('Brady Nolin');
+	console.log('Brady Nolin');
 
-    function onAjaxifyEnd() {
-        if (ajaxify.data.template.name !== 'topic' || ajaxify.data.tid !== tid) {
-            closeModal();
-            $(window).off('action:ajaxify.end', onAjaxifyEnd);
-        }
-    }
+	function onAjaxifyEnd() {
+		if (ajaxify.data.template.name !== 'topic' || ajaxify.data.tid !== tid) {
+			closeModal();
+			$(window).off('action:ajaxify.end', onAjaxifyEnd);
+		}
+	}
 
-    function deletePosts(btn, route) {
-        btn.attr('disabled', true);
-        Promise.all(postSelect.pids.map(pid => api.del(route(pid), {})))
-            .then(closeModal)
-            .catch(alerts.error)
-            .finally(() => {
-                btn.removeAttr('disabled');
-            });
-    }
+	function deletePosts(btn, route) {
+		btn.attr('disabled', true);
+		Promise.all(postSelect.pids.map(pid => api.del(route(pid), {})))
+			.then(closeModal)
+			.catch(alerts.error)
+			.finally(() => {
+				btn.removeAttr('disabled');
+			});
+	}
 
-    function showPostsSelected() {
-        if (postSelect.pids.length) {
-            modal.find('#pids').translateHtml('[[topic:fork-pid-count, ' + postSelect.pids.length + ']]');
-        } else {
-            modal.find('#pids').translateHtml('[[topic:fork-no-pids]]');
-        }
-    }
+	function showPostsSelected() {
+		if (postSelect.pids.length) {
+			modal.find('#pids').translateHtml('[[topic:fork-pid-count, ' + postSelect.pids.length + ']]');
+		} else {
+			modal.find('#pids').translateHtml('[[topic:fork-no-pids]]');
+		}
+	}
 
-    function checkButtonEnable() {
-        if (postSelect.pids.length) {
-            deleteBtn.removeAttr('disabled');
-            purgeBtn.removeAttr('disabled');
-        } else {
-            deleteBtn.attr('disabled', true);
-            purgeBtn.attr('disabled', true);
-        }
-    }
+	function checkButtonEnable() {
+		if (postSelect.pids.length) {
+			deleteBtn.removeAttr('disabled');
+			purgeBtn.removeAttr('disabled');
+		} else {
+			deleteBtn.attr('disabled', true);
+			purgeBtn.attr('disabled', true);
+		}
+	}
 
-    function closeModal() {
-        if (modal) {
-            modal.remove();
-            modal = null;
-            postSelect.disable();
-        }
-    }
+	function closeModal() {
+		if (modal) {
+			modal.remove();
+			modal = null;
+			postSelect.disable();
+		}
+	}
 
-    return DeletePosts;
+	return DeletePosts;
 });
 
