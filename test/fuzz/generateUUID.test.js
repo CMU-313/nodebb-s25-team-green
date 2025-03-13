ca832e5bf12aa891b30de30940323fb3b82e4909
Patrice <pbortey@andrew.cmu.edu>
2025-03-12 20:47:33 -0400

    resolved lint errors

--- test/fuzz/generateUUID.test.js
+++ test/fuzz/generateUUID.test.js
@@ -1,13 +1,18 @@
-const { expect } = require("chai");
-const utils = require("../../src/utils"); // Using require to prevent ESM issues
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable indent */
 
-describe("Fuzz Test: utils.generateUUID()", function () {
-    it("should generate valid UUIDs consistently", function () {
+'use strict';
+
+const { expect } = require('chai');
+const utils = require('../../src/utils'); // Using require to prevent ESM issues
+
+describe('Fuzz Test: utils.generateUUID()', function () {
+    it('should generate valid UUIDs consistently', function () {
         for (let i = 0; i < 100; i++) { // Run 100 tests
             const uuid = utils.generateUUID();
 
             // Ensure the result is a string
-            expect(uuid).to.be.a("string");
+            expect(uuid).to.be.a('string');
 
             // Ensure the UUID matches the standard format
             expect(uuid).to.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
